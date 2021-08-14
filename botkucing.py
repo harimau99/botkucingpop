2facd05e5e8ca88b35ebb71fa9a5ab0c3199a151
Jo Jaafar <johar.jaafar@razer.com>
2021-08-14 22:43:47 +0800

    botkucing.py

--- botkucing.py
+++ botkucing.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
-
-driver = webdriver.Edge("lib\edgedriver_win32\msedgedriver.exe")
-driver.get("https://popcat.click/")
-
-while True:
-    driver.execute_script('const app=document.querySelector("#app").__vue__;function click(){app.accumulator=800,app.bot=!1,app.sequential_max_pops=0,pop()}function pop(){for(var e=0;e<=800;e++)setTimeout(()=>{d
+import time
+def main():
+    #driverPath = r"/Users/<yourpath^secondary/Documents/liker/chromedriver_mac"
+    driver = webdriver.Chrome(executable_path="<your path>") # Chrome
+    driver.get("https://popcat.click/")
+    while True: 
+        driver.find_element_by_xpath("//*[@id='app']").click()
+main()
